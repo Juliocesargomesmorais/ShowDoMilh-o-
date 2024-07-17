696c7a547aa7227d156b914d53117d9d7b11abb6
Dam Alert <157314608+Juliocesargomesmorais@users.noreply.github.com>
2024-07-17 20:10:22 -0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,350 @@
+
+let perguntas_Nvl1 = [
+    {
+        pergunta: "Qual obra é considerada o principal marco do Classicismo em Portugal?",
+        escolha1: '"Auto da Barca do Inferno" de Gil Vicente ',
+        escolha2: '"Os Lusíadas" de Camões ',
+        escolha3: '"Sentimento do Mundo" de Carlos Drummond de Andrade',
+        escolha4: '"Memórias Póstumas de Brás Cubas" de Machado de Assis',
+        resposta: 2
+    },
+    {
+        pergunta: "Quem foi o autor da primeira obra de quinhentismo da literatura brasileira?",
+        escolha1: 'Gregório de Matos ',
+        escolha2: 'Pedro Vaz de Caminha ',
+        escolha3: 'Padre Antônio Vieira ',
+        escolha4: "Tomás Antônio Gonzaga",
+        resposta: 2
+    },
+    {
+        pergunta: "Qual é a característica marcante da poesia lírica do Classicismo?",
+        escolha1: 'Uso da sátira como forma de crítica social ',
+        escolha2: 'Ênfase na idealização amorosa e na natureza ',
+        escolha3: 'Abordagem de temas mitológicos greco-romanos ', 
+        escolha4:'Valorização dos costumes populares e da oralidade',
+        resposta: 2
+    },
+    {
+        pergunta: "Quem é considerado o maior poeta do Classicismo em Portugal?",
+        escolha1: "Camões ",
+        escolha2: "Gil Vicente ",
+        escolha3: "Sá de Miranda ",
+        escolha4: "Luís Vaz de Camões",
+        resposta: 4
+    },
+    {
+        pergunta: "Qual é o tema central da obra 'Os Lusíadas' de Luís de Camões?",
+        escolha1: 'A colonização do Brasil ',
+        escolha2: 'A descoberta do caminho marítimo para as Índias ',
+        escolha3: 'As navegações portuguesas ',
+        escolha4: 'A lenda do Rei Artur',
+        resposta: 3
+    },
+    {
+        pergunta: "Qual é a característica principal da poesia lírica do Classicismo?",
+        escolha1: ' Uso do soneto como forma poética ',
+        escolha2: 'Ênfase na exaltação dos feitos heróicos;',
+        escolha3: 'Abordagem de temas ligados à mitologia grega ', 
+        escolha4: 'Crítica social e política',
+        resposta: 1
+    },
+    {
+        pergunta: "Qual alternativa apresenta corretamente uma forma no modo imperativo negativo?",
+        escolha1: '"Fala mais alto, por favor."',
+        escolha2: '"Não saia sem avisar."',
+        escolha3: '"Traga-me um café, por favor."',
+        escolha4: '"Corra para alcançar o ônibus!"',
+        resposta: 2
+    },
+    {
+        pergunta: 'O verbo no modo imperativo pode ter função de:',
+        escolha1: "elogiar",
+        escolha2: "falar sobre fenômenos da natureza",
+        escolha3: "ordem",
+        escolha4: "justificar",
+        resposta: 3
+    },
+    {
+        pergunta: 'Em qual alternativa o verbo está conjugado no presente do subjuntivo? ',
+        escolha1: '"Se ele vier, avise-me." ',
+        escolha2: '"Eu vou falar com ele quando você chegar." ',
+        escolha3: '"Se eu tivesse dinheiro, compraria aquele carro." ',
+        escolha4: '"Ela dirá a verdade se você pedir."',
+        resposta: 1
+    }
+];
+
+let perguntas_Nvl2 = [
+    {
+        pergunta: 'Qual é o tempo verbal da frase: "Ela estudará para a prova amanhã."? ',
+        escolha1: 'Futuro do presente ',
+        escolha2: 'Futuro do pretérito ', 
+        escolha3: 'Presente do indicativo ',
+        escolha4: 'Pretérito perfeito do indicativo', 
+        resposta: 1
+    },
+    {
+        pergunta: 'Qual verbo na 2° pessoa do singular está no modo subjuntivo do pretérito perfeito?',
+        escolha1: 'vivesses',
+        escolha2: 'morrerias',
+        escolha3: 'andarás ', 
+        escolha4: "cortastes", 
+        resposta: 1
+    },
+    {
+        pergunta: 'Qual foi a principal forma de expressão literária no Quinhentismo brasileiro? ',
+        escolha1: 'Romances de cavalaria ',
+        escolha2: 'Poesia épica ',
+        escolha3: 'Poesia lírica ', 
+        escolha4: 'Crônicas de viagem', 
+        resposta: 4
+    },
+    {
+        pergunta: 'Em qual alternativa o verbo está conjugado no pretérito imperfeito do subjuntivo? ',
+        escolha1: '"Se eu estudava mais, passaria no vestibular." ',
+        escolha2: '"Eu andei pela cidade durante a tarde." ',
+        escolha3: '"Que ele estude bastante para a prova." ', 
+        escolha4: '"Quando você terminar, me avise."',
+        resposta: 3
+    },
+    {
+        pergunta: 'Qual o principal objetivo dos cronistas que registraram a chegada dos portugueses ao Brasil no Quinhentismo? ',
+        escolha1: 'Exaltar os feitos dos navegadores portugueses ',
+        escolha2: 'Documentar a flora e fauna brasileiras ',
+        escolha3: 'Descrever os costumes e culturas dos povos nativos ',
+        escolha4: 'Criticar a política colonial portuguesa',
+        resposta: 3
+    },
+    {
+        pergunta: 'Qual é o tempo verbal da frase: "Ele havia terminado o trabalho antes do prazo."? ',
+        escolha1: 'Pretérito perfeito do indicativo ',
+        escolha2: 'Pretérito mais-que-perfeito simples do indicativo ',
+        escolha3: 'Pretérito imperfeito do indicativo ',
+        escolha4: 'Futuro do presente', 
+        resposta: 2
+    },
+    {
+        pergunta: 'Quais são as principais características formais da poesia de Camões, autor de "Os Lusíadas"? ',
+        escolha1: 'Redondilha maior e poesia épica ',
+        escolha2: 'Verso decassílabo e soneto',
+        escolha3: 'Trovadorismo e verso livre ',
+        escolha4: 'Poesia lírica e verso hendecassílabo',
+        resposta: 2
+    },
+    {
+        pergunta: 'Qual das alternativas abaixo apresenta uma característica predominante do Classicismo em relação à forma de expressão literária?',
+        escolha1: 'Utilização de versos irregulares e livres.',
+        escolha2: 'Predomínio da sátira como principal recurso crítico.',
+        escolha3: 'Valorização da simplicidade e clareza na linguagem poética.',
+        escolha4: 'Ênfase na descrição exata de paisagens naturais.',
+        resposta: 3
+    }
+];
+
+let perguntas_Nvl3 = [
+    {
+        pergunta: 'Complete a frase com o verbo “vir” no pretérito imperfeito do subjuntivo para a 2ª pessoa do plural:“Se vós ____ mais cedo, poderíamos ter começado a reunião.”',
+        escolha1: 'viestes',
+        escolha2: 'víreis',
+        escolha3: 'viésseis ',
+        escolha4: 'viésseis',
+        resposta:  3
+    },
+    {
+        pergunta: 'Complete o texto com a conjugação correta do verbo “haver” no pretérito mais-que-perfeito do indicativo:“Quando chegamos à festa, nós já __________ (haver) visto aquele filme.”',
+        escolha1: 'havíamos', 
+        escolha2: 'havemos',
+        escolha3: 'houvemos',
+        escolha4: 'houvéramos',
+        resposta: 4
+    },
+    {
+        pergunta: 'Qual é o tempo verbal da frase: "Eles teriam vindo se tivessem sido convidados."? ',
+        escolha1: 'Pretérito perfeito do indicativo ',
+        escolha2: 'Pretérito mais-que-perfeito composto do indicativo ',
+        escolha3: 'Futuro do presente ',
+        escolha4: 'Futuro do pretérito',
+        resposta: 2
+    },
+    {
+        pergunta: 'Qual obra é considerada o marco inicial do Quinhentismo brasileiro?',
+        escolha1: 'Carta de Pero Vaz de Caminha',
+        escolha2: 'Prosopopeia de Bento Teixeira',
+        escolha3: 'Auto da Barca do Inferno',
+        escolha4: 'Tratado da Terra do Brasil', 
+        resposta: 1
+    },
+    {
+        pergunta: 'Qual é a principal característica formal da poesia trovadoresca?',
+        escolha1: 'Uso da métrica fixa do decassílabo',
+        escolha2: 'Uso da forma fixa do soneto',
+        escolha3: "'Ênfase na exaltação dos feitos heroicos'",
+        escolha4: 'Uso de estrofes de oito versos com versos setissilabos e redondilha maior',
+        resposta: 4
+    },
+    {
+        pergunta: 'Qual é o verbo na 3ª pessoa do singular no pretérito imperfeito do subjuntivo?',
+        escolha1: 'estudaria',
+        escolha2: 'vivêssemos',
+        escolha3: 'fizesses', 
+        escolha4: 'cantasse', 
+        resposta: 4
+    },
+    {
+        pergunta: 'Qual é o tema central da obra "Os Lusíadas" de Luís de Camões?',
+        escolha1: 'A expansão territorial portuguesa na África',
+        escolha2: 'A descoberta do caminho marítimo para as Índias',
+        escolha3: 'A fundação de Portugal como nação independente',
+        escolha4: 'A guerra contra os mouros',
+        resposta: 2
+    }
+];
+
+let perguntas_Nvl4 = [
+    {
+        pergunta: 'Qual é o verbo na 1ª pessoa do singular no presente do subjuntivo?',
+        escolha1: 'fizesse',
+        escolha2: 'falasse',
+        escolha3: 'faço',
+        escolha4: 'estude',
+        resposta: 4
+    },
+    {
+        pergunta: 'Qual é o principal gênero literário do Classicismo em Portugal?',
+        escolha1: 'Teatro', 
+        escolha2: 'Poesia',
+        escolha3: 'Romance',
+        escolha4: 'Crônica',
+        resposta: 2
+    },
+    {
+        pergunta: 'Qual é o verbo na 3ª pessoa do plural no modo indicativo presente?',
+        escolha1: 'estudam',
+        escolha2: 'estudem',
+        escolha3: 'estudem',
+        escolha4: 'estudariam',
+        resposta: 1
+    },
+    {
+        pergunta: 'Qual escola literária portuguesa do século XVI é caracterizada pela valorização da simplicidade e pela imitação dos modelos clássicos greco-romanos?',
+        escolha1: 'Barroco',
+        escolha2: 'Romantismo',
+        escolha3: 'Arcadismo',
+        escolha4: 'Classicismo',
+        resposta: 4
+    }
+];
+
+const premios = [0, 1000, 2000, 3000, 4000, 5000, 10000, 20000, 30000, 40000, 50000, 100000, 200000, 300000, 400000, 500000, 1000000];
+
+let pergunta = document.getElementById('pergunta');
+let escolha1 = document.getElementById('btn1');
+let escolha2 = document.getElementById('btn2');
+let escolha3 = document.getElementById('btn3');
+let escolha4 = document.getElementById('btn4');
+let pontos = document.getElementById('pontos');
+let acerto = document.getElementById('acerto');
+let erro = document.getElementById('erro');
+let pulou = document.getElementById('pulou');
+let cont = 0;
+let aleatorio = 0;
+let nivel = perguntas_Nvl1;
+let premioAcumulado = premios[cont];
+let respostaAtual = 0;
+let contPulos = 0;
+let timer = 90;
+
+//Cria a pergunta aleatoriamente e a exclui da lista de perguntas 
+function criaPergunta (){
+    timer = 90;
+    nivel = verificaNivel();
+    aleatorio = random(nivel);
+    var p = nivel[aleatorio];
+    pergunta.innerHTML = "<h1>" + p.pergunta + "</h1>";
+    escolha1.innerHTML = "<p>" + p.escolha1 + "</p>";
+    escolha2.innerHTML = "<p>" + p.escolha2 + "</p>";
+    escolha3.innerHTML = "<p>" + p.escolha3 + "</p>";
+    escolha4.innerHTML = "<p>" + p.escolha4 + "</p>";
+    respostaAtual = p.resposta;
+    nivel.splice(aleatorio, 1);
+}
+
+//randomizar as perguntas DISPONIVEIS
+function random(nivel){
+    let aleatorio = Math.floor(Math.random() * nivel.length);
+    return aleatorio;
+}
+
+//Verificar o nivel da pergunta, e devolve o array que será utilizado
+function verificaNivel(){
+    if(cont < 5){
+        nivel = perguntas_Nvl1;
+    }
+    else if(cont < 10){
+        nivel = perguntas_Nvl2;
+    }
+    else if(cont < 15){
+        nivel = perguntas_Nvl3;
+    }
+    else if(cont === 15){
+        alert("PRÓXIMA PERGUNTA VALE 1 MILHÃO");
+        nivel = perguntas_Nvl4;
+    }
+    else{
+        alert("PARABÉNS, VOCÊ ACABA DE SE TORNAR UM MILIONÁRIO!!!🎉🎉🎉");
+        return window.location.assign("index.html");
+    }
+    return nivel;
+}
+
+//Verificar se a resposta esta correta/adiciona mais um ao contador nesse caso
+function verificaResposta(escolha){
+    //Pergunta "ESTA CERTO DISSO?"
+    if(escolha === respostaAtual){
+        cont++;
+        premioAcumulado = premios[cont];
+        pontos.innerHTML = "Você possui R$"+ premioAcumulado + ",00";
+        erro.innerHTML = "Se errar ganha R$"+ premioAcumulado/2 + ",00"; 
+        acerto.innerHTML = "Pergunta valendo R$"+ premios[cont+1] + ",00";
+        criaPergunta();
+    }   
+    else{
+        gameOverLose();
+    }
+}
+
+//Pula a questão e a retira do array
+function pular(){
+    if(contPulos < 3){
+        contPulos++;
+        pulou.innerHTML = "Pular" + contPulos + "/3";
+        criaPergunta();
+    }
+    else{
+        alert('PULO ESGOTADO');
+    }
+}
+
+function gameOverWin(){
+    alert("Parabéns, você ganhou R$" + premioAcumulado + ",00");
+    return window.location.assign("index.html");
+}
+
+function gameOverLose(){
+    alert("Oh não, parece que a respota está errada, mas pelo menos você levou R$" + premioAcumulado/2 + ",00");
+    return window.location.assign("index.html");
+}
+
+//Contador de tempo para dar a resposta
+function timeOut(){
+    setInterval(() => {
+        tempo.innerHTML = "Tempo: " + timer-- + "s";
+        if (timer == -2){
+            alert("TEMPO ESGOTADO! Infelizmente o tempo acabou, mas pelo menos você ganhou R$" + premioAcumulado/2 + ",00");
+            return window.location.assign("index.html");
+        }
+    }, 1000);
+}
+
+criaPergunta();
+timeOut();
